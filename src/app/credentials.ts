334775c1a791cd217dac3931b122d8bb1ca06129
mulleady96 <32593466+mulleady96@users.noreply.github.com>
2018-09-17 15:44:42 +0100

    Update credentials.ts

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export var config = {
-  apiKey: "AIzaSyCDj8OTRlerNIEO1f2e-HF6-YfS4lAsTxc",
-     authDomain: "ionicmap-8b1d0.firebaseapp.com",
-     databaseURL: "https://ionicmap-8b1d0.firebaseio.com",
-     projectId: "ionicmap-8b1d0",
-     storageBucket: "ionicmap-8b1d0.appspot.com",
-     messagingSenderId: "943024581452"
+  apiKey: "",
+     authDomain: "",
+     databaseURL: "",
+     projectId: "",
+     storageBucket: "",
+     messagingSenderId: ""
   };
